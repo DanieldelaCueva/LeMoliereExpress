ca20565ac83b689d30b03d23297ff3b65f53b7bc
DanieldelaCueva <76701720+DanieldelaCueva@users.noreply.github.com>
2021-02-07 12:13:12 +0100

    Initial commit

---
+++ php/registraremails.php
@@ -0,0 +1,25 @@
+<?php
+$servidor="sql7.freemysqlhosting.net";
+$user="sql7390835";
+$password="lDTffr2xsY";
+$db="sql7390835";
+
+$conexion= new mysqli($servidor, $user, $password, $db);
+
+if($conexion->connect_error){
+die("CONEXION FALLIDA " . $conexion->connect_error);
+}
+
+if(isset($_POST['nombre']) and isset($_POST['apellidos']) and isset($_POST['email']) and isset($_POST['calidad'])){
+$texto = $_POST['texto'];
+$apellidos = $_POST['apellidos'];
+$email = $_POST['email'];
+$calidad = $_POST['calidad'];
+
+$sql="INSERT INTO recibir_periodico (nombre, apellidos, email, calidad) VALUES ('$nombre', '$apellidos', '$email', '$calidad')";
+
+if($conexion->query($sql)==true){
+<script>window.history.back();<script>
+}
+}
+?>
